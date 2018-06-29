3ae9ad82042627a29866dc0e969ead24c4d21899
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-06-29 14:45:19 +0200

    Fix: Remove CompositeAttributeConverter

--- src/main/kotlin/de/difuture/ekut/pht/entity/converter/TrainIdentifierConverter.kt
+++ src/main/kotlin/de/difuture/ekut/pht/entity/converter/TrainIdentifierConverter.kt
@@ -1,50 +1,41 @@
 package de.difuture.ekut.pht.entity.converter
 
 import de.difuture.ekut.pht.api.TrainIdentifier
-import org.neo4j.ogm.typeconversion.CompositeAttributeConverter
+import de.difuture.ekut.pht.lib.core.constants.NA
+import org.neo4j.ogm.typeconversion.AttributeConverter
 import java.net.URI
 
 
-class TrainIdentifierConverter : CompositeAttributeConverter<TrainIdentifier> {
+class TrainIdentifierConverter : AttributeConverter<TrainIdentifier, String> {
 
 
     companion object {
 
-        private const val KEY_REPOSITORY = "repository"
-        private const val KEY_REGISTRY_URI = "registryURI"
+        // Because $ is invalid in URIs and repository names
+        const val sep = "$"
     }
 
-    override fun toGraphProperties(value: TrainIdentifier?): MutableMap<String, *> {
+    override fun toGraphProperty(value: TrainIdentifier?): String {
 
-        val result : MutableMap<String, String> = HashMap()
-        if (value != null) {
+        if (value == null) {
 
-            result[KEY_REPOSITORY] = value.repository
-            result[KEY_REGISTRY_URI] = value.registryURI.toASCIIString()
+            return NA
         }
-        return result
+        return value.registryURI.toASCIIString() + sep + value.repository
     }
 
-    override fun toEntityAttribute(value: MutableMap<String, *>?): TrainIdentifier? {
+    override fun toEntityAttribute(value: String?): TrainIdentifier? {
 
-        // map is null or at least one of the keys is missing
-        if (value == null || KEY_REPOSITORY !in value.keys || KEY_REGISTRY_URI !in value.keys) {
-
-            return null
-        }
-        val repo = value[KEY_REPOSITORY].toString()
-        val uri  = value[KEY_REGISTRY_URI].toString()
-
-        if (repo.isBlank() || uri.isBlank()) {
+        if (value == null) {
 
             return null
         }
+         val spt = value.split("$")
 
-        // URI
-        return try {
-            TrainIdentifier(URI.create(uri), repo)
-        } catch (e : Exception) {
+        if (spt.size != 2) {
+
             return null
         }
+        return try {TrainIdentifier(URI.create(spt[0]), spt[1]) } catch (e : Exception) { null }
     }
 }
